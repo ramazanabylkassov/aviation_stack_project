4d3c40f935ec3660957259c724d0fdb20533858f
Your Name <you@example.com>
2024-04-03 20:09:36 +0500

    test

--- dags/test.py
+++ dags/test.py
@@ -14,48 +14,4 @@ def test_dag_import():
     dag_bag = DagBag(dag_folder='dags/', include_examples=False)
     assert dag_bag.import_errors == {}, "DAG import failed"
 
-@pytest.fixture
-def mock_dependencies():
-    """Mock the dependencies of the task."""
-    with patch('airflow.models.TaskInstance.are_dependencies_met') as mock_dependencies_met:
-        mock_dependencies_met.return_value = True
-        yield
-
-@pytest.fixture
-def mock_bigquery_client():
-    """Mock the BigQuery Client."""
-    with patch('google.cloud.bigquery.Client') as MockClient:
-        # Mock any method of BigQuery client that your DAG's tasks call
-        mock_client_instance = Mock()
-        MockClient.return_value = mock_client_instance
-        yield mock_client_instance
-
-def test_task_execution(mock_bigquery_client):
-    """Task Execution Test focusing on a specific task within a DAG."""
-    # Import the DAG from your project
-    execution_date = datetime.now(pytz.utc)
-
-    with create_session() as session:
-        dag_run = DagRun(
-            dag_id='FlightsETL_pytest',
-            run_id='test_run',
-            execution_date=execution_date,
-            start_date=execution_date,
-            state=State.RUNNING,
-            run_type=DagRunType.MANUAL
-        )
-        session.add(dag_run)
-        session.commit()
-
-    # Retrieve the specific task
-    task = my_dag.get_task('BIGQUERY_raw_to_datamart')
-    
-    # Simulate execution of the task
-    ti = TaskInstance(task=task, run_id='test_run')
-    context = ti.get_template_context()
-    ti.run(ignore_ti_state=True)  # Set ignore_ti_state to True to not check the previous state of the TaskInstance
-    
-    # Verify the task succeeded
-    assert ti.state == 'success', "Task execution failed"
-
 # update
