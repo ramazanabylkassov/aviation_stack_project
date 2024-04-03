50d257e3beeda36ac52ec24458861751d1bc395f
Your Name <you@example.com>
2024-04-03 21:31:41 +0500

    asd

--- dags/temp_dag.py
+++ dags/temp_dag.py
@@ -21,15 +21,15 @@ dag = DAG(
     schedule_interval="0 1 * * *", 
 )
 
-# cit_api_to_gcs = PythonOperator(
-#     task_id = f"cit_api_to_gcs",
-#     python_callable=api_to_gcs,
-#     op_kwargs={
-#         'ds': '{{ ds }}', 
-#         'iata': f'cit'
-#         },
-#     dag=dag
-# )
+cit_api_to_gcs = PythonOperator(
+    task_id = f"cit_api_to_gcs",
+    python_callable=api_to_gcs,
+    op_kwargs={
+        'ds': '{{ ds }}', 
+        'iata': f'cit'
+        },
+    dag=dag
+)
 
 cit_gcs_to_bigquery = PythonOperator(
     task_id = f"cit_gcs_to_bigquery",
@@ -53,6 +53,6 @@ raw_to_datamart = PythonOperator(
     dag=dag
 )
 
-cit_gcs_to_bigquery >> raw_to_datamart
+cit_api_to_gcs >> cit_gcs_to_bigquery >> raw_to_datamart
 
 # Update 47
